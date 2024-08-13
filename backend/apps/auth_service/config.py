dc7309591529b35a02c5e055fd4d003defa65a59
Evgeny <jacksonou3@gmail.com>
2024-08-13 21:46:14 +0300

    Добавлены методы для кодирования и декодирования jwt при помощи одноименной библиотеки

--- backend/apps/auth_service/config.py
+++ backend/apps/auth_service/config.py
@@ -1,20 +1,16 @@
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent.parent
-print(BASE_DIR)
 
 class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "core" / "_certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "core" / "_certs" / "jwt-public.pem"
-    def __repr__(self):
-        return f"AuthJWT(private_key_path={self.private_key_path})"
+    algorithm: str = "RS256"
 
-# Создаем экземпляр класса
-auth_jwt_instance = AuthJWT()
+class Settings(BaseSettings):
+    auth_jwt: AuthJWT = AuthJWT()
 
-# Выводим значение private_key_path
-print(f"private_key_path: {auth_jwt_instance.private_key_path}")
+settings = Settings()
 
-# Вызываем метод __repr__
-print(repr(auth_jwt_instance))
