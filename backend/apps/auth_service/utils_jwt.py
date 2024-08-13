b1fd05608f2c6b1a4f506c4e886d0808e33ff5e0
Evgeny <jacksonou3@gmail.com>
2024-08-13 21:46:29 +0300

    Добавлены методы для кодирования и декодирования jwt при помощи одноименной библиотеки

---
+++ backend/apps/auth_service/utils_jwt.py
@@ -0,0 +1,19 @@
+import jwt
+import starlette
+from config import settings
+
+def encode_jwt(payload, private_key, algorithm):
+    encoded = jwt.encode(
+        payload,
+        private_key=settings.auth_jwt.private_key_path.read_text(),
+        algorithm=settings.auth_jwt.algorithm,
+    )
+    return encoded
+
+def decode_jwt(token, public_key, algorithm):
+    decoded = jwt.encode(
+        token,
+        public_key=settings.auth_jwt.public_key_path.read_text(),
+        algorithm=settings.auth_jwt.algorithm,
+    )
+    return decoded
