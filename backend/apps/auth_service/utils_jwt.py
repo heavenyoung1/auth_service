03f2ad7cc829bcc3267f6aca604ec665e0f3fe60
Evgeny <jacksonou3@gmail.com>
2024-08-14 18:33:31 +0300

    Изменен метод кодирования JWT для соответствия классу

--- backend/apps/auth_service/utils_jwt.py
+++ backend/apps/auth_service/utils_jwt.py
@@ -1,13 +1,20 @@
 import bcrypt
 import jwt
-import starlette
 from config import settings
+from datetime import timedelta, datetime
 
 def encode_jwt(
-                payload: dict,
-                private_key: settings.auth_jwt.private_key_path.read_text(),
-                algorithm: settings.auth_jwt.algorithm,
-                ):
+    payload: dict,
+    private_key: settings.auth_jwt.private_key_path.read_text(),
+    algorithm: settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+):
+    to_encode = payload.copy()
+    expire = ...
+    to_encode.update(exp=expire)
+    now = datetime.now()
+
     encoded = jwt.encode(
         payload,
         private_key,
