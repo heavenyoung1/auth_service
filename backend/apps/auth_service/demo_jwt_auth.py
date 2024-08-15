7388b1deb6f56880dd273a6f0dee5cd93f1662a0
Evgeny <jacksonou3@gmail.com>
2024-08-15 11:08:09 +0300

    Метод валидации пользователя настроен

--- backend/apps/auth_service/demo_jwt_auth.py
+++ backend/apps/auth_service/demo_jwt_auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, HTTPException, status
 
 from schemas import UserSchema
 import utils_jwt as auth_util
@@ -28,8 +28,27 @@ users_db: dict[str, UserSchema] = {
     sam.username: sam,
 }
 
-def validate_auth_user():
-    pass
+def validate_auth_user(
+    username: str = Form(),
+    password: str = Form(),
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="invalid username or password"
+    )
+    if (user := users_db.get(username)):
+        raise unauthed_exc
+
+    if auth_util.validate_password(
+        password=password,
+        hashed_password=user.password
+    ):
+        raise unauthed_exc
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="user inactive",
+        )
 
 @router.post("/login", response_model=TokenInfo)
 def auth_user_issue_jwt(user: UserSchema = Depends(validate_auth_user())):
