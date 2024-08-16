e2a0775f735da111a88b99a3c234e1ded828751a
Evgeny <jacksonou3@gmail.com>
2024-08-16 10:16:04 +0300

    импорт роутера

--- backend/apps/main.py
+++ backend/apps/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 
+from auth_service.demo_jwt_auth import router
+
 app = FastAPI(
-    title="Booking Service",
-    description="Creator HeavenYoung",
-    version="0.0.1",
-)
+        title="Booking Service",
+        description="Creator HeavenYoung",
+        version="0.0.1",
+    )
+
+app.include_router(router)
 
 @app.get("/")
 def root():
