b927f7887b52a3fdc91f78eb997b13fb354a95a3
Evgeny <jacksonou3@gmail.com>
2024-08-19 15:34:57 +0300

    Минорные изменения

--- backend/apps/auth_service/auth.py
+++ backend/apps/auth_service/auth.py
@@ -2,25 +2,11 @@ from typing import Annotated
 
 from fastapi import Depends, APIRouter
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import APIRouter, Depends, HTTPException, status
 
-from pydantic import BaseModel
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 router = APIRouter()
 
-class LoginSchema(BaseModel):
-    username: str
-    password: str
-
-# Пример схемы для ответа
-class TokenSchema(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
-
-@router.post("/login", response_model=TokenSchema)
-async def login(user: LoginSchema):
-    if user.username != "test" or user.password != "secret":
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-        )
+@router.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
--- backend/apps/main.py
+++ backend/apps/main.py
@@ -2,14 +2,9 @@ from fastapi import FastAPI
 import uvicorn
 from auth_service.auth import router as auth_router
 
+app = FastAPI()
 
-app = FastAPI(
-        title="Booking Service",
-        description="Creator HeavenYoung",
-        version="0.0.1",
-    )
-
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
 
 @app.get("/")
 def root():
