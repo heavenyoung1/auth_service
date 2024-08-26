abb78ec7462b6ad8b5e907a15ea25e35fbc5a491
Evgeny <jacksonou3@gmail.com>
2024-08-26 14:16:03 +0300

    Настроено подключение к БД PostgreSQL

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,16 +1,15 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQL_ALCHEMY_DATABASE_URL = "postgresql+asyncpg://username:password@localhost:5432/testpostgreserver"
 
-engine = create_engine(
+engine = create_async_engine(
     SQL_ALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False,
-                            autoflush=False,
-                            bind=engine,
-                            expire_on_commit=False)
+async_session_maker = sessionmaker(autocommit=False,
+                                   autoflush=False,
+                                   expire_on_commit=False)
 
 Base = declarative_base()
