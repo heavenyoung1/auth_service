ba1764fce1f8548af61499b258c0a27199c221ee
Evgeny <jacksonou3@gmail.com>
2024-08-28 10:12:15 +0300

    Настроен alembic alembic revision & alembic upgrade head работают

--- backend/core/database.py
+++ backend/core/database.py
@@ -1,1 +1,29 @@
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
 
+
+
+DATABASE_URL = 'postgresql+asyncpg://postgres:postgres@localhost:5432/testpostgreserver'
+
+Base: DeclarativeMeta = declarative_base()
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    pass
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = sessionmaker(
+                engine,
+                class_=AsyncSession,
+                expire_on_commit=False,
+)
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
+
+async def get_user_db(session = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
