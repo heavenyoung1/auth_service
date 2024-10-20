fe723bd42c7b53f8d5399e9f1f0abcce3048d184
Evgeny <jacksonou3@gmail.com>
2024-10-20 11:26:15 +0300

    Настройка схем и аутентификации, исправление названий методов

--- backend/apps/auth_service/main.py
+++ backend/apps/auth_service/main.py
@@ -1,4 +1,21 @@
+import uuid
+
 from fastapi import FastAPI
+from fastapi_users import FastAPIUsers
+from auth import auth_backend
+from user_manager import get_user_manager
+
+from database import User
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager,
+    [auth_backend],
+)
 
 app = FastAPI()
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
--- backend/apps/auth_service/user_manager.py
+++ backend/apps/auth_service/user_manager.py
@@ -17,5 +17,11 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
         print((f"User {user.id}  has registered"))
 
     async def on_after_forgot_password(self, user: User, token: str, request: Optional[Request] = None):
-        print(f"User {user.id} has forgot theit password. Reset token: {token}")
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(self, user: User, token: str, request: Optional[Request] = None):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
 
