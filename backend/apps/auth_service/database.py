51c81dec97ee56f0c73fdc40eb4976c172af4fe9
Evgeny <jacksonou3@gmail.com>
2024-10-25 19:14:40 +0300

    Добавлены новые роуты

--- backend/apps/auth_service/database.py
+++ backend/apps/auth_service/database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from fastapi import Depends
 from models import User
 from base import Base
-from fastapi_users import FastAPIUsers
-import uuid
-from user_manager import get_user_manager
-from auth import auth_backend
+# from fastapi_users import FastAPIUsers
+# import uuid
+# from user_manager import get_user_manager
+# from auth import auth_backend
 
 from typing import AsyncGenerator
 
@@ -31,5 +31,5 @@ async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
 
 
-fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
-current_active_user = fastapi_users.current_user(active=True)
+# fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
+# current_active_user = fastapi_users.current_user(active=True)
--- backend/apps/auth_service/main.py
+++ backend/apps/auth_service/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi_users import FastAPIUsers
 from auth import auth_backend
 from user_manager import get_user_manager
+from schemas import UserCreate, UserRead, UserUpdate
 
 from database import User
 
@@ -19,6 +20,27 @@ app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth/jwt",
     tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
 )
 
 if __name__ == "__main__":
