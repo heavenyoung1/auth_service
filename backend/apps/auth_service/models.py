4633173f58dd9e9642373c9bc8d196373f1c25d6
Evgeny <jacksonou3@gmail.com>
2024-10-25 21:16:14 +0300

    Добавлен метод для User database

--- backend/apps/auth_service/models.py
+++ backend/apps/auth_service/models.py
@@ -1,7 +1,19 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
 from .base import Base
 from .mixins.id_int_pk import IdIntPkMixin
+from typing import TYPE_CHECKING
+
+from fastapi_users.db import (
+    SQLAlchemyUserDatabase,
+    SQLAlchemyBaseUserTable
+)
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 class User( Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
     pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
 
