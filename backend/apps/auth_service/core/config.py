864de3eb0b4568eb2f16804eaeca99be1ad43d2a
Evgeny <jacksonou3@gmail.com>
2024-10-26 22:26:42 +0300

    настроен класс для вынесения настройки lifetome_seconds

---
+++ backend/apps/auth_service/core/config.py
@@ -0,0 +1,10 @@
+# from pydantic import BaseModel
+# from pydantic_settings import BaseSettings
+#
+# class AccessToken(BaseModel):
+#     lifetime_seconds: int = 3600
+#
+# class Settings(BaseSettings):
+#     access_token: AccessToken = AccessToken()
+#
+# settings = Settings()
