e43ecb5a446b94205c17e5db42e1b0c2f85beac6
Evgeny <jacksonou3@gmail.com>
2024-10-27 19:15:41 +0300

    fastapi_users_routes сделан

---
+++ backend/apps/auth_service/dependencies/fastapi_users_routes.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from ..models.user import User
+from .user_manager import get_user_manager
+from ..authentification import auth_backend
+from ..types.user_id import UserIdType
+
+fastapi_users = FastAPIUsers[User, UserIdType](
+    get_user_manager,
+    [auth_backend],
+)
