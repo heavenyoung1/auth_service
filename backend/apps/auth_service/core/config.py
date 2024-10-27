e722e1b45527d2fb9e235aab3b9c4f99d469a048
Evgeny <jacksonou3@gmail.com>
2024-10-27 19:49:32 +0300

    Правки при помощи Black

--- backend/apps/auth_service/core/config.py
+++ backend/apps/auth_service/core/config.py
@@ -2,10 +2,17 @@ from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from pydantic import PostgresDsn
 
+
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
     reset_password_token_secret: str
     verification_token_secret: str
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+    auth: str = "/auth"
+
 
 class DataBaseConfig(BaseModel):
     url: PostgresDsn
@@ -14,11 +21,12 @@ class DataBaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 10
 
+
 class Settings(BaseSettings):
     access_token: AccessToken = AccessToken()
     db: DataBaseConfig = DataBaseConfig()
-    env_prefix="APP_CONFIG__"
-
+    env_prefix = "APP_CONFIG__"
+    api: ApiPrefix = ApiPrefix()
 
 
 settings = Settings()
