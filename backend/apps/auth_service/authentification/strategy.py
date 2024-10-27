a7a0a19712c0bdf94557d94f3503fc698d0898c4
Evgeny <jacksonou3@gmail.com>
2024-10-27 13:37:47 +0300

    настройка lifetime_second ссылается на settings

--- backend/apps/auth_service/authentification/strategy.py
+++ backend/apps/auth_service/authentification/strategy.py
@@ -1,7 +1,7 @@
 from fastapi import Depends
 from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
 from typing import TYPE_CHECKING
-from core.config import settings
+from ..core.config import settings
 
 if TYPE_CHECKING:
     from ..models.access_token import AccessToken
@@ -9,4 +9,4 @@ if TYPE_CHECKING:
 def get_database_strategy(
     access_token_db: AccessTokenDatabase["AccessToken"] = Depends(get_access_token_db),
 ) -> DatabaseStrategy:
-    return DatabaseStrategy(access_token_db, lifetime_seconds=3600)
+    return DatabaseStrategy(access_token_db, settings.access_token.lifetime_seconds)
