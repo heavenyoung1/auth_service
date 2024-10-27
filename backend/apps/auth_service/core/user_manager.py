fd0a575de9e90a5d501f9563a57d7b363333571a
Evgeny <jacksonou3@gmail.com>
2024-10-27 17:44:50 +0300

    изменения в структуре

---
+++ backend/apps/auth_service/core/user_manager.py
@@ -0,0 +1,37 @@
+import logging
+import uuid
+from typing import Optional
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, UUIDIDMixin, IntegerIDMixin
+
+from ..models.user import User
+from ..dependencies.users import get_user_db
+
+from ..types.user_id import UserIdType
+
+SECRET = "SECRET"
+
+log = logging.getLogger(__name__)
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(
+            self, user: User, request: Optional[Request] = None
+    ):
+        log("User %r has registered", user.id)
+
+    async def on_after_forgot_password(
+            self, user: User, token: str, request: Optional[Request] = None
+   ):
+        log("User %r has forgot their password. Reset token: %r", user.id, token)
+
+    async def on_after_request_verify(
+            self, user: User, token: str, request: Optional[Request] = None
+    ):
+        log("Verification requested for user %r. Verification token: %r", user.id, token)
+
+
+
