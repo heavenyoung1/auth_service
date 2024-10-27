3b333dded58b8af2cfb4d362a15c2dc0482c410c
Evgeny <jacksonou3@gmail.com>
2024-10-27 18:37:53 +0300

    навёл порядок при помощи BLACK

--- backend/apps/auth_service/core/user_manager.py
+++ backend/apps/auth_service/core/user_manager.py
@@ -1,6 +1,7 @@
 import logging
 import uuid
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
+
 
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, UUIDIDMixin, IntegerIDMixin
@@ -14,24 +15,44 @@ SECRET = "SECRET"
 
 log = logging.getLogger(__name__)
 
+if TYPE_CHECKING:
+    from fastapi import Request
+
+
 class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
 
     async def on_after_register(
-            self, user: User, request: Optional[Request] = None
+        self,
+        user: User,
+        request: "Optional[Request]" = None,
     ):
-        log("User %r has registered", user.id)
+        log.warning(
+            "User %r has registered",
+            user.id,
+        )
 
     async def on_after_forgot_password(
-            self, user: User, token: str, request: Optional[Request] = None
-   ):
-        log("User %r has forgot their password. Reset token: %r", user.id, token)
+        self,
+        user: User,
+        token: str,
+        request: "Optional[Request]" = None,
+    ):
+        log.warning(
+            "User %r has forgot their password. Reset token: %r",
+            user.id,
+            token,
+        )
 
     async def on_after_request_verify(
-            self, user: User, token: str, request: Optional[Request] = None
+        self,
+        user: User,
+        token: str,
+        request: "Optional[Request]" = None,
     ):
-        log("Verification requested for user %r. Verification token: %r", user.id, token)
-
-
-
+        log.warning(
+            "Verification requested for user %r. Verification token: %r",
+            user.id,
+            token,
+        )
