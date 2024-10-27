4ec0797a2256edad8ddcf3a0e45703bc42fb6920
Evgeny <jacksonou3@gmail.com>
2024-10-27 10:35:47 +0300

    добавлен класс для взаимодействия с БД

--- backend/apps/auth_service/core/config.py
+++ backend/apps/auth_service/core/config.py
@@ -1,10 +1,17 @@
-# from pydantic import BaseModel
-# from pydantic_settings import BaseSettings
-#
-# class AccessToken(BaseModel):
-#     lifetime_seconds: int = 3600
-#
-# class Settings(BaseSettings):
-#     access_token: AccessToken = AccessToken()
-#
-# settings = Settings()
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn
+
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+
+class Settings(BaseSettings):
+    access_token: AccessToken = AccessToken()
+    db: DataBaseConfig = DataBaseConfig()
+
+
+
+settings = Settings()
