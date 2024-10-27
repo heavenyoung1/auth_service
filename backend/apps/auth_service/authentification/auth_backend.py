dca1f2fa5577b97c1e55f23af19fab3fce90f76b
Evgeny <jacksonou3@gmail.com>
2024-10-27 18:39:09 +0300

    исправление

--- backend/apps/auth_service/authentification/auth_backend.py
+++ backend/apps/auth_service/authentification/auth_backend.py
@@ -6,5 +6,5 @@ from strategy import get_database_strategy
 authentification_backend = AuthenticationBackend(
     name="access-token-db",
     transport=bearer_transport,
-    get_strategy=get_database_strategy(),
+    get_strategy=get_database_strategy,
 )
