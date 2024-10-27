5cf95426a85afc2a4339a3e7c279cc64fcdaddfa
Evgeny <jacksonou3@gmail.com>
2024-10-27 19:00:09 +0300

    создана зависимости user_manager

--- backend/apps/auth_service/core/__init__.py
+++ backend/apps/auth_service/core/__init__.py
@@ -1,9 +1,10 @@
 __all__ = (
     "settings",
     "DataBaseHelper",
-    "db_helper"
-
+    "db_helper",
+    "UserManager",
 )
 
 from config import settings
 from db_helper import DataBaseHelper, db_helper
+from ..core.user_manager import UserManager
--- backend/apps/auth_service/dependencies/user_manager.py
+++ backend/apps/auth_service/dependencies/user_manager.py
@@ -1,2 +1,19 @@
-async def get_user_manager(user_db=Depends(get_user_db)):
-    yield UserManager(user_db)
+from typing import Annotated
+
+from typing import TYPE_CHECKING
+from ..core.user_manager import UserManager
+from fastapi import Depends
+
+from .users import get_user_db
+
+if TYPE_CHECKING:
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+
+
+async def get_user_manager(
+    users_db: Annotated[
+        "SQLAlchemyUserDatabase",
+        Depends(get_user_db),
+    ],
+):
+    yield UserManager(users_db)
