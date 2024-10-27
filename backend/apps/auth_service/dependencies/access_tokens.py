f2124217cfe835d76be1af65abbb95b47504d188
Evgeny <jacksonou3@gmail.com>
2024-10-27 12:10:24 +0300

    Сделана функция для получения токена

--- backend/apps/auth_service/dependencies/access_tokens.py
+++ backend/apps/auth_service/dependencies/access_tokens.py
@@ -1,4 +1,15 @@
+from typing import TYPE_CHECKING, Annotated
+from ..core.db_helper import db_helper
+from ..models.access_token import AccessToken
+from fastapi import Depends
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
 async def get_access_token_db(
-    session: AsyncSession = Depends(get_async_session),
+    session: Annotated[
+        "AsyncSession",
+        Depends(db_helper.session_getter)
+    ],
 ):
-    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+    yield AccessToken.get_db(session=session)
