9e0b714e5a9d88e91c9864e124050a631a3f7b09
Evgeny <jacksonou3@gmail.com>
2024-10-27 11:47:09 +0300

    Настроено получение пользователей

--- backend/apps/auth_service/dependencies/users.py
+++ backend/apps/auth_service/dependencies/users.py
@@ -1,13 +1,11 @@
 from typing import TYPE_CHECKING, Annotated
-from ..database import async_session_maker
+from ..core.db_helper import db_helper
+from ..models.user import User
 
 from fastapi import Depends
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-async def get_user_db(
-        session: Annotated[
-            "AsyncSession",
-        ] = Depends(async_session_maker.get)):
-    yield SQLAlchemyUserDatabase(session, User)
+async def get_user_db(session: Annotated["AsyncSession", Depends(db_helper.session_getter)]):
+    yield User.get_db(session=session)
