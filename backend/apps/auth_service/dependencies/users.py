3fe4e77daf1520796f800278eb42ff7ae9996db1
Evgeny <jacksonou3@gmail.com>
2024-10-27 12:10:40 +0300

    Апдейст get_user_db

--- backend/apps/auth_service/dependencies/users.py
+++ backend/apps/auth_service/dependencies/users.py
@@ -7,5 +7,10 @@ from fastapi import Depends
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
-async def get_user_db(session: Annotated["AsyncSession", Depends(db_helper.session_getter)]):
+async def get_user_db(
+    session: Annotated[
+        "AsyncSession",
+        Depends(db_helper.session_getter)
+    ],
+):
     yield User.get_db(session=session)
