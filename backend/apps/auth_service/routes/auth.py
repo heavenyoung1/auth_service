6be0aad0fa3c93c46e62aecba4c96584dea26829
Evgeny <jacksonou3@gmail.com>
2024-11-08 15:51:06 +0300

    Импорты - абсолютный -> относительный

--- backend/apps/auth_service/routes/auth.py
+++ backend/apps/auth_service/routes/auth.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
-from ..core.config import settings
-from ..dependencies.fastapi_users_configuration import fastapi_users
-from ..authentification.auth_backend import authentification_backend
+
+# from ..core.config import settings
+# from ..dependencies.fastapi_users_configuration import fastapi_users
+# from ..authentification.auth_backend import authentification_backend
+
+from core.config import settings
+from dependencies.fastapi_users_configuration import fastapi_users
+from authentification.auth_backend import authentification_backend
 
 auth_router = APIRouter(
     prefix=settings.api.auth,
