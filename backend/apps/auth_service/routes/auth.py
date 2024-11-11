93bd72d8c668d8e345c95a06a9d3350eeed5e65c
Evgeny <jacksonou3@gmail.com>
2024-11-11 16:50:52 +0300

    Добавлен новый router для создания и чтения пользователя

--- backend/apps/auth_service/routes/auth.py
+++ backend/apps/auth_service/routes/auth.py
@@ -3,12 +3,25 @@ from fastapi import APIRouter
 from core.config import settings
 from dependencies.fastapi_users_configuration import fastapi_users
 from authentification.auth_backend import authentification_backend
+from schemas.user import UserCreate, UserRead
+
 
 auth_router = APIRouter(
     prefix=settings.api.auth,
     tags=["Auth"],
 )
 
+# /login
+# /logout
 auth_router.include_router(
-    router=fastapi_users.get_auth_router(authentification_backend),
+    router=fastapi_users.get_auth_router(
+        authentification_backend
+    ),
+)
+
+auth_router.include_router(
+    fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
 )
