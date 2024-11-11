4d7c3548d6a746de3e7dc4988c37bbc4152a756e
Evgeny <jacksonou3@gmail.com>
2024-11-11 16:00:56 +0300

    Исправлен импорт, чтобы не было циклических, так он точнее

--- backend/apps/auth_service/dependencies/fastapi_users_configuration.py
+++ backend/apps/auth_service/dependencies/fastapi_users_configuration.py
@@ -2,7 +2,7 @@ from fastapi_users import FastAPIUsers
 
 from models.user import User
 from dependencies.user_manager import get_user_manager
-from authentification import auth_backend
+from authentification.auth_backend import authentification_backend as auth_backend
 from app_types.user_id import UserIdType
 
 fastapi_users = FastAPIUsers[User, UserIdType](
