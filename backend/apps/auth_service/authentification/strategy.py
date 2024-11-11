aa7b4e74a810f9290a36934c1285baf523301233
Evgeny <jacksonou3@gmail.com>
2024-11-11 15:46:40 +0300

    Исправление импортов

--- backend/apps/auth_service/authentification/strategy.py
+++ backend/apps/auth_service/authentification/strategy.py
@@ -1,12 +1,16 @@
 from fastapi import Depends
 from typing import Annotated
-from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
 from typing import TYPE_CHECKING
-from ..dependencies.access_tokens import get_access_token_db
-from ..core.config import settings
+from dependencies.access_tokens import get_access_token_db
+from core.config import settings
 
 if TYPE_CHECKING:
     from ..models.access_token import AccessToken
+
 
 def get_database_strategy(
     access_token_db: Annotated[
