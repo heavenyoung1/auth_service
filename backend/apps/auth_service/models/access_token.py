5159fd742b9485f07ad288133848740319250e52
Evgeny <jacksonou3@gmail.com>
2024-11-11 15:45:29 +0300

    Исправление импорта

--- backend/apps/auth_service/models/access_token.py
+++ backend/apps/auth_service/models/access_token.py
@@ -8,7 +8,7 @@ from sqlalchemy import Integer, ForeignKey
 from typing import TYPE_CHECKING
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..app_types.user_id import UserIdType
+from app_types.user_id import UserIdType
 from sqlalchemy.orm import Mapped, mapped_column
 
 if TYPE_CHECKING:
