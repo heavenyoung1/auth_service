d43aca2d0daf6b31676cecedeabca1445d517cde
Evgeny <jacksonou3@gmail.com>
2024-12-06 16:58:24 +0300

    Обновление конфигурации для работы с Users

--- backend/apps/auth_service/routes/__init__.py
+++ backend/apps/auth_service/routes/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "auth_router",
     "router",
+    "users_router"
 )
 
 from routes.auth import auth_router
 from routes.router import router
+from routes.users import router as users_router
--- backend/apps/auth_service/routes/users.py
+++ backend/apps/auth_service/routes/users.py
@@ -12,8 +12,9 @@ router = APIRouter(
 )
 
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
-    prefix="/users",
-    tags=["users"],
+    router=fastapi_users.get_users_router(
+        UserRead,
+        UserUpdate,
+    ),
 )
 
