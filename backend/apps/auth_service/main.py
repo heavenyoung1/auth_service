cb0256345072474cf98b28fdb113639abb232427
Evgeny <jacksonou3@gmail.com>
2025-04-14 13:32:12 +0300

    Первый коммит будущего API авторизации пользователей

--- backend/apps/auth_service/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-from routes.router import router
-
-app = FastAPI()
-app.include_router(router)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, log_level="info")
