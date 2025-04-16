5b0fa8f79f6565cc9d7bd373721e5c5aaf4c47be
Evgeny <jacksonou3@gmail.com>
2025-04-16 15:14:07 +0300

    создана модель User, добавлены отладочные элементы

--- app/database/db.py
+++ app/database/db.py
@@ -3,3 +3,4 @@ from sqlalchemy import create_engine
 engine = create_engine("ЗДЕСЬ ВПИШИ СВОЮ БД ПОЗЖЕ", echo=True)
 URL = "СТРОКА ДЛЯ ПОДКЛЮЧЕНИЯ БД"
 # Позже выбери один из вариантов создания движка
+# create_engine.echo = True (регистрация всех инструкций, есть также False/debug)
--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,23 @@
-from sqlalchemy import Integer, String, ForeignKey
-from sqlalchemy.orm import DeclarativeBase
-Base = DeclarativeBase()
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import Optional
+
+class Base(DeclarativeBase):
+    pass
 
 class User(Base):
     __tablename__ = "user"
-    id = 
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+    # Для отладки определяется метод repr
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+# Для тестов
+user = User(id=1, name="Ivan", fullname="Ivan Petrov")
 
 User.__tablename__
 User.__mapper__
