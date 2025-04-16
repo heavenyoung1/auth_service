7e0ef7083a990b0b906ef5c7ab25faa392b70437
Evgeny <jacksonou3@gmail.com>
2025-04-16 16:49:59 +0300

    рефакторинг

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+url = "sqlite+pysqlite:///:memory:"
+connect_args = {"echo: True"}
+
+engine = create_engine(url, connect_args=connect_args)
 
 Session = sessionmaker(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
