b09f633ab71368f562364f18cb625db11da94a83
Evgeny <jacksonou3@gmail.com>
2025-04-25 15:10:13 +0300

    дополнительные условия для валидации схем (минимальная длина логина - 6  символов

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,7 +4,9 @@ from app.models.user import Role
 # python -m app.schemas.user - команда для тестирования работы модуля как пакета
 
 class UserBase(BaseModel):
-    login: str
+    login: str = Field(min_length=6, max_length=32,
+                       example="qwerty",
+                       description=f"Логин должен быть от 6 до 32 символов")
     fullname: str
 
 class UserCreate(UserBase):
@@ -28,3 +30,8 @@ if __name__ == "__main__":
     user_create = UserCreate(login="testUser", fullname="Piter Parker", password="123456", role=Role.ADMIN)
     print(user_create)
 
+# Тест валидации данных 
+try:
+    invalid_user = UserCreate(login="i", fullname="Test", password="123", role=Role.USER)
+except ValueError as e:
+    print(f"Ошибка валидации данных {e}")
