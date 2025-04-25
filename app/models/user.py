4eaa8a49db78116765e8c3c2a47c2ed08c1cbdda
Evgeny <jacksonou3@gmail.com>
2025-04-25 15:12:10 +0300

    Дополнительная проверка в модели (минимальная длина логина)

--- app/models/user.py
+++ app/models/user.py
@@ -14,7 +14,7 @@ class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    login: Mapped[str] = mapped_column(String(30))
+    login: Mapped[str] = mapped_column(String(30), nullable=False, info={"min_length": 6}) # Минимальная длина - 6 символов
     fullname: Mapped[str] = mapped_column(String(100))
     hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
     role: Mapped[Role] = mapped_column(default=Role.USER, nullable=False) #Enum(Role) можно удалить (далее), нужен для прозрачной работы с БД и валидации данных 
