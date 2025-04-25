1018c8b481cbdf63fb60aa93f435ff579cd06cea
Evgeny <jacksonou3@gmail.com>
2025-04-25 13:53:08 +0300

    Добавление схем для валидации данных User

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,3 +1,12 @@
+from pydantic import BaseModel
+from models.user import Role
 
+class UserBase(BaseModel):
+    login: str
+    fullname: str
+
+class UserCreate(UserBase):
+    password: str
+    role: Role
 
 
