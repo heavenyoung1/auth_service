c065c2042590a6e1281ac348785b1d9ad5e8d81b
Eugene <jacksonou3@gmail.com>
2025-04-26 11:25:32 +0300

    Добавлен тест для неверного пароля, добавлена функция для хеширования пароля

--- app/core/security.py
+++ app/core/security.py
@@ -9,3 +9,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # Проверка пароля - сравнение простого и хешированного паролей
 def verify_password(original_password, hashed_password):
     return pwd_context.verify(original_password, hashed_password)
+
+# Хеширование пароля
+def get_password_to_hash(password):
+    return pwd_context.hash(password)
+
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,17 @@
 import pytest
 from app.core.security import pwd_context, verify_password
 
+# Проверка пароля - успешная
 def test_verify_password_success():
     # 1. Хешируем тестовый пароль
     hashed_password = pwd_context.hash("test_password")
 
     # 2. Проверяем, что функция возвращает True для верного пароля
     assert verify_password("test_password", hashed_password) is True
+
+def test_verify_password_failure():
+    # 1. Хешируем тестовый пароль
+    hashed_password = pwd_context.hash("test_password")
+
+    # 2. Проверяем, что функция возвращает False для неверного пароля
+    assert verify_password("wrong_password", hashed_password) is False
