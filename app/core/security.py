728ff8f962db21dd2004e5282e5258e9354dd1b8
Eugene <jacksonou3@gmail.com>
2025-04-26 11:10:25 +0300

    Ренейминг переменных

--- app/core/security.py
+++ app/core/security.py
@@ -6,6 +6,6 @@ from app.core.config import settings
 # Объект для безопасного хеширования и проверки паролей с помощью библиотеки passlib
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-#
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+# Проверка пароля - сравнение простого и хешированного паролей
+def verify_password(original_password, hashed_password):
+    return pwd_context.verify(original_password, hashed_password)
