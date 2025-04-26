38330b55bf0c8d72383324092d0db93c4b6ca7b8
Eugene <jacksonou3@gmail.com>
2025-04-26 11:41:58 +0300

    Добавлены fixture для удобства переиспользования данных в тестах

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,25 +1,30 @@
 import pytest
 from app.core.security import pwd_context, verify_password
 
+# Фикстура pytest - для переиспользования тестовых данных
+@pytest.fixture
+def test_password():
+    return "test_password"
+
 # Проверка пароля - успешная
-def test_verify_password_success():
+def test_verify_password_success(test_password):
     # Хешируем тестовый пароль
-    hashed_password = pwd_context.hash("test_password")
+    hashed_password = pwd_context.hash(test_password)
 
     # Проверяем, что функция возвращает True для верного пароля
-    assert verify_password("test_password", hashed_password) is True
+    assert verify_password(test_password, hashed_password) is True
 
 # Проверка пароля - НЕуспешная
-def test_verify_password_failure():
+def test_verify_password_failure(test_password):
     # Хешируем тестовый пароль
-    hashed_password = pwd_context.hash("test_password")
+    hashed_password = pwd_context.hash(test_password)
 
     # Проверяем, что функция возвращает False для неверного пароля
     assert verify_password("wrong_password", hashed_password) is False
 
 # Проверка пустого пароля
-def test_verify_password_empty():
+def test_verify_password_empty(test_password):
     # Хешируем тестовый пароль
-    hashed_password = pwd_context.hash("test_password")
+    hashed_password = pwd_context.hash(test_password)
 
     assert verify_password("", hashed_password) is False
