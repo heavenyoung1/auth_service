368f54fd2a3d95754927758eb271bfffddc9ea13
Eugene <jacksonou3@gmail.com>
2025-04-26 09:29:26 +0300

    Добавление класса Settings, где вынесено управление настройками FastApi приложения. Так же добавлен файл .env и gitignore, куда вынесен .env

---
+++ app/api/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+        env_file_endcoding = "utf-8"
+
+settings = Settings()
