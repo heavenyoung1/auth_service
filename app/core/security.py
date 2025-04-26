1926da2589b74d4a17b9f277f87630d5b028fc88
Eugene <jacksonou3@gmail.com>
2025-04-26 10:56:47 +0300

    Добавление зависимостей, написан pwd_context и функция верификации пароля (сравнение простого и хешированного паролей)

---
+++ app/core/security.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from app.core.config import settings
+
+# Объект для безопасного хеширования и проверки паролей с помощью библиотеки passlib
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+#
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password)
