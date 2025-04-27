ff5144ceca017f8b762b9be6bba16da91c09405b
Eugene <jacksonou3@gmail.com>
2025-04-27 14:47:48 +0300

    Рефакторинг и исправление ошибок

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from logging import Logger, getLogger
 
 from app.schemas.user import UserCreate, Token
 from app.database.db import get_session
@@ -8,13 +9,24 @@ from app.core.security import get_password_to_hash, create_access_token
 
 
 
-router = APIRouter
+router = APIRouter()
 
 @router.post("/register", response_model=Token)
-def register(user_in: UserCreate, session: Session = Depends(get_session)) -> User:
+def register(
+            user_in: UserCreate, 
+            session: Session = Depends(get_session),
+            logger: Logger = Depends(getLogger)
+):
+    # Проверка существования пользователя
     db_user = session.query(User).filter(User.login == user_in.login).first()
     if db_user:
-        raise HTTPException(status_code=400, detail="Такой логин уже существует")
+        raise HTTPException(status_code=400, detail="Такой логин уже существует.")
+    
+    # Создание нового пользователя
+
+    if len(user_in.password) < 3:
+        raise HTTPException(status_code=400, detail="Пароль должен быть минимум 4 символа.")
+
     hashed_password = get_password_to_hash(user_in.password)
     db_user = User(
                     login=user_in.login,
@@ -22,8 +34,11 @@ def register(user_in: UserCreate, session: Session = Depends(get_session)) -> Us
                     hashed_password=hashed_password,
                     role=user_in.role
                     )
+    
     session.add(db_user)
     session.commit()
     session.refresh()
+
+    # Генеарация токена
     access_token = create_access_token(data={"sub": user_in.login})
     return {"access_token": access_token, "token_type": "bearer"}
