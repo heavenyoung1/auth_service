63a6f784f3dda43f23cc158533cd7a2196d9d83c
Eugene <jacksonou3@gmail.com>
2025-05-01 00:28:15 +0300

    Написан тест для создания пользователя, и почищен код от прошлых тестов

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,8 @@
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 from app.database.db import get_session, init_db
-from app.models.user import Base 
+from app.models.user import Base, User, Role
 
 # Используем тестовую БД
 TEST_DATABASE_URL = "postgresql+psycopg2://postgres:P%40ssw0rd@192.168.31.168:5432/auth_test_db"
@@ -16,43 +16,42 @@ def setup_db():
     yield() # Зачем здесь это?
     Base.metadata.drop_all(bind=engine)
 
+# Подключение к БД при помощи SQLAlchemy
 def test_sqlalchemy_connection(setup_db: tuple[()]):
     with engine.connect() as connection:
         assert connection is not None
+        print("Успешное подключение к PostgreSQL через SQLAlchemy!")
 
+    # Получение версии PostgreSQL
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT version();")) # Как вывести данные
+        version = result.fetchone()[0]
+        print(f"Версия PostgreSQL: {version}")
 
+    # Получение списка баз данных
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT datname FROM pg_database;")) # что такое pg_database
+        database = [row[0] for row in result]
+        print("Список баз данных")
+        for db in database:
+            print(f"БД: {db}")
 
+def test_create_user(setup_db):
+    with next(get_session()) as session:
+        # Создание тестового пользователя
+        user = User(
+            login="testlogin",
+            fullname="Test User",
+            hashed_password="password",
+            role=Role.USER
+        )
 
+        # Сохранение в БД
+        session.add(user)
+        session.commit()
 
-# @pytest.fixture(scope="function")
-# def setup_db():
-#     init_db()  # Создаем таблицы
-#     yield
-#     Base.metadata.drop_all(bind=engine)  # Удаляем после теста
-
-# def test_sqlalchemy_connection(setup_db):
-#     with engine.connect() as connection:
-#         assert connection is not None, "Не удалось подключиться к базе данных"
-#         print("✅ Успешное подключение к PostgreSQL через SQLAlchemy!")
-
-#     with engine.connect() as connection:
-#         result = connection.execute(text("SELECT version();"))
-#         version = result.fetchone()[0]
-#         print(f"PostgreSQL version: {version}")
-
-#     with engine.connect() as connection:
-#         result = connection.execute(text("SELECT datname FROM pg_database;"))
-#         databases = [row[0] for row in result]
-#         print("\nСписок баз данных:")
-#         for db in databases:
-#             print(f"- {db}")
-
-# def test_create_user(setup_db):
-#     with next(get_session()) as session:
-#         user = User(login="testlogin", fullname="Test User", hashed_password="hashed", role=Role.USER)
-#         session.add(user)
-#         session.commit()
-#         db_user = session.query(User).filter(User.login == "testlogin").first()
-#         assert db_user is not None, "Пользователь не был создан"
-#         assert db_user.login == "testlogin"
-#         print(f"Метод __repr__: {db_user}")
+        # Проверка создания пользователя (что он был создан)
+        db_user = session.query(User).filter(User.login == "testlogin").first() # Обязательно ли first()?
+        assert db_user is not None, "Пользователь не был создан"
+        assert db_user.login == "testlogin"
+        print(f"Метод __repr__: {db_user}")
