7c5be2a56d6d27b0376c8e9336790cde0d8c663f
Eugene <jacksonou3@gmail.com>
2025-04-30 18:36:32 +0300

    добавление описания

--- app/main.py
+++ app/main.py
@@ -3,8 +3,12 @@ from app.api.v1.auth import router as auth_router
 from fastapi import FastAPI
 import uvicorn
 
-app = FastAPI()
-app.include_router(auth_router, prefix="/api/v1")
+app = FastAPI(
+            title="Тестовый сервис авторизации",
+            description="Креатор, Фаундер и прочие челы с горы",
+            version="0.1",
+)
+app.include_router(auth_router, prefix="/API/v0.1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
