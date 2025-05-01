0b6f11f9995d71723cd3beada90515c8503e7b9b
Eugene <jacksonou3@gmail.com>
2025-05-01 11:46:35 +0300

    Рефакторинг тестов

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,14 @@
-import pytest
-from fastapi.testclient import TestClient
 from app.main import app
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from app.models.user import Base
 from app.database.db import get_session
 
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
+import pytest
+
+# Настройка тестовой Базы Данных
 TEST_DATABASE_URL = "postgresql+psycopg2://postgres:P%40ssw0rd@192.168.31.168:5432/auth_test_db"
 engine = create_engine(TEST_DATABASE_URL, echo=True)
 TestingSession = sessionmaker(engine)
@@ -24,11 +26,18 @@ def client(setup_db):
     def override_get_session():
         with TestingSession() as session:
             yield session
+
+    # Подменяем зависимость для тестов
     app.dependency_overrides[get_session] = override_get_session
+
+    # Создаем тестовый клиент
     with TestClient(app) as client:
         yield client
+
+    # Очищаем переопределения после теста
     app.dependency_overrides.clear()
 
+# Тест успешной регистрации пользователя
 def test_register_success(client):
     response = client.post("/API/v0.1/register", json={
         "login": "testuser",
@@ -42,6 +51,7 @@ def test_register_success(client):
     assert response.status_code == 200
     assert "access_token" in response.json()
 
+# Тест попытки регистрации с уже существующим логином
 def test_register_duplicate_login(client):
     client.post("/API/v0.1/register", json={
         "login": "testuser",
@@ -51,6 +61,7 @@ def test_register_duplicate_login(client):
         }
     )
 
+    # Попытка повторной регистрации с тем же логином
     response = client.post("/API/v0.1/register", json={
         "login": "testuser",
         "fullname": "Test User",
@@ -62,11 +73,12 @@ def test_register_duplicate_login(client):
     assert response.status_code == 400
     assert response.json()["detail"] == "Такой логин уже существует."
 
+# Тест валидации короткого пароля (менее 4 символов)
 def test_register_short_password(client):
     response = client.post("/API/v0.1/register", json={
         "login": "testuser",
         "fullname": "Test User",
-        "password": "000",
+        "password": "000", # Слишком короткий пароль (3 символа)
         "role": "user"
         }
     )
