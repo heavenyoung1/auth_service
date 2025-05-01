9518bc64eea50df24301ffcb6b967256225bc877
Eugene <jacksonou3@gmail.com>
2025-05-01 09:32:51 +0300

    удаление ненужных тестовых скриптов

--- tests/test_config.py
+++
@@ -1,8 +0,0 @@
-from app.core.config import settings
-
-def test_config():
-    print(f"DATABASE_URL: {settings.DATABASE_URL}")
-    assert settings.DATABASE_URL == "postgresql+psycopg2://postgres:P@ssw0rd@192.168.31.168:5432/auth_db"
-
-if __name__ == "__main__":
-    test_config()
