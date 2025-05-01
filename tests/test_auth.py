09c2c9ebe69f8df5f95765605e0ffb1692e8a7aa
Eugene <jacksonou3@gmail.com>
2025-05-01 10:24:45 +0300

    Написана фикстура для подключения тестового клиента и переопределения get_session в тестовую сессию

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,11 +4,12 @@ from app.main import app
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from app.models.user import Base
+from app.database.db import get_session
 
 
 TEST_DATABASE_URL = "postgresql+psycopg2://postgres:P%40ssw0rd@192.168.31.168:5432/auth_test_db"
 engine = create_engine(TEST_DATABASE_URL, echo=True)
-Session = sessionmaker(engine)
+TestingSession = sessionmaker(engine)
 
 @pytest.fixture(scope="function")
 def setup_db():
@@ -17,3 +18,13 @@ def setup_db():
     yield
     # Удаляем таблицы после теста
     Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture
+def client(setupt_db):
+    def override_get_session():
+        with TestingSession() as session:
+            yield session
+    app.dependency_overrides[get_session] = override_get_session
+    with TestClient(app) as client:
+        yield client
+    app.dependency_overrides.clear()
