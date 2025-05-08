bebffa68dfe20a693379cd311310c44c50e57595
Eugene <jacksonou3@gmail.com>
2025-05-08 22:38:09 +0300

    Добавление новых контсант

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import "./style.css";
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
+const signInForm = document.getElementById("sign-in-form");
+const signUpForm = document.getElementById("sign-up-form");
 
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
