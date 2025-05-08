2e734ebc26498c6e76738d936d95ab32df208b54
Eugene <jacksonou3@gmail.com>
2025-05-08 23:15:51 +0300

    Обработка формы регистрации + исправление названия переменных для двух методов

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,10 +15,26 @@ signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
 
+// Обработка формы регистрации
+signInForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const login = document.getElementById("register-login").value;
+  const username = document.getElementById("register-name").value;
+  const password = document.getElementById("register-password").value;
+  const responseElement = document.getElementById("register-response");
+  responseElement.textContent = "Загрузка..."
+
+  try {
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ login, username, password }),
+    });
+
 // Обработка формы авторизации
 signInForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const username = document.getElementById("login-login").value;
+  const login = document.getElementById("login-login").value;
   const password = document.getElementById("login-password").value;
   const responseElement = document.getElementById("login-response");
   responseElement.textContent = "Загрузка..."
@@ -27,7 +43,7 @@ signInForm.addEventListener("submit", async (e) => {
     const response = await fetch("/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ login, password }),
     });
   const data = await response.json();
   responseElement.textContent = data.message || data.detail || "Успех!";
