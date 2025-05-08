d70d47cd02d9dd5be98d504c9c82dc8481cf60e1
Eugene <jacksonou3@gmail.com>
2025-05-09 00:52:02 +0300

    Создана функция для обработки ошибок

--- frontend/utils/errorHandler.js
+++ frontend/utils/errorHandler.js
@@ -5,3 +5,23 @@ const errorLocalization = {
     "value is not a valid enumeration member": "Недопустимая роль",
     "field required": "обязательное поле",
   };
+
+  export const processorErrorMessages = (detail) => {
+    if (!Array.isArray(detail)) return detail;
+
+    return detail
+    .map((error) => {
+        const field = error.loc[error.loc.length - 1];
+        let message = error.msg;
+        const localized = Object.entries(errorLocalization).find(([key]) =>
+        message.includes(key)
+        );
+        if (localized) {
+        message = `${field} ${localized[1]}`;
+        } else if (message === "invalid credentials") {
+        message = "Неверные учетные данные";
+        }
+        return message;
+    })
+    .join("; ");
+    };
