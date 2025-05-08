c5068ac9e1c7af7206a73758b7d613dfd2ab26ee
Eugene <jacksonou3@gmail.com>
2025-05-08 23:06:37 +0300

    Обработка формы авторизации

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,10 +6,34 @@ const container = document.getElementById("container");
 const signInForm = document.getElementById("sign-in-form");
 const signUpForm = document.getElementById("sign-up-form");
 
+// Обработка слайда формы регистрации/авторизации
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
+});
+
+// Обработка формы авторизации
+signInForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = document.getElementById("login-login").value;
+  const password = document.getElementById("login-password").value;
+  const responseElement = document.getElementById("login-response");
+  responseElement.textContent = "Загрузка..."
+
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+  const data = await response.json();
+  responseElement.textContent = data.message || data.detail || "Успех!";
+  responseElement.style.color = data.message ? "green" : "red";
+} catch (error) {
+  responseElement.textContent = "Ошибка: " + error.message;
+  responseElement.style.color = "red";
+}
 });
