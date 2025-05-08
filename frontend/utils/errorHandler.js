dde784f00fc1334171a5a25ef8fef4b901f681c5
Eugene <jacksonou3@gmail.com>
2025-05-09 00:50:03 +0300

    Добавлен словарь с ошибками, файл вынесен в отдельную директорию

---
+++ frontend/utils/errorHandler.js
@@ -0,0 +1,7 @@
+// Мапа для локализации ошибок
+const errorLocalization = {
+    "String should have at least 6 characters": "должен содержать минимум 6 символов",
+    "String should have at least 4 characters": "должен содержать минимум 4 символа",
+    "value is not a valid enumeration member": "Недопустимая роль",
+    "field required": "обязательное поле",
+  };
