023bba7eae521a62c1eb832c48f81d9fc81868f0
Evgeny <jacksonou3@gmail.com>
2025-05-14 13:23:38 +0300

    Добавление fetch

--- frontend/setupTests.mjs
+++ frontend/setupTests.mjs
@@ -3,3 +3,6 @@ import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+// Добавляем fetch в глобальную среду
+global.fetch = require('node-fetch');
