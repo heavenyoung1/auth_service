168f3a9efec83db07d2d587637072f0a196462d5
Evgeny <jacksonou3@gmail.com>
2025-05-15 11:28:58 +0300

    ✨(models): Добавлена новая модель RefreshToken для последующей реализации logout

---
+++ backend/app/models/token.py
@@ -0,0 +1,16 @@
+from sqlalchemy import String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from datetime import datetime
+
+class Base(DeclarativeBase):
+    pass
+
+class RefreshToken(Base):
+    __tablename__ = "refreshToken"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    token: Mapped[str] = mapped_column(String, unique=True , nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"))
+    created_at: Mapped[datetime] = mapped_column(DateTime)
+
+    user = relationship("User", back_populates="refreshToken")
