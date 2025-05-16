a7bb316d5ebab89bd495d608c694ba99947096e4
Evgeny <jacksonou3@gmail.com>
2025-05-16 09:43:42 +0300

    Удалены артефакты от frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from app.api.v1.auth import router as auth_router
@@ -12,15 +11,6 @@ app = FastAPI(
 
 app.include_router(auth_router, prefix="/API/v0.1")
 app.include_router(test_router, prefix="/API/v0.1")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Адрес фронтенда
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
