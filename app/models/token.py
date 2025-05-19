c6558839da74d3166b9fd950d62da3dd4d6bcccc
Evgeny <jacksonou3@gmail.com>
2025-05-19 13:19:17 +0300

    Добавление доп.условия для поля Refresh-Token, created_at = nullable(false)

--- app/models/token.py
+++ app/models/token.py
@@ -10,7 +10,7 @@ class RefreshToken(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     token: Mapped[str] = mapped_column(String, unique=True , nullable=False)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), index=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
     expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     user: Mapped["User"] = relationship(back_populates="refresh_tokens")
 
