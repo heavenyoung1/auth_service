ce612c55656169747d77b203d64df58260673349
Evgeny <jacksonou3@gmail.com>
2025-05-19 09:23:40 +0300

    Добавлена Инициализация таблиц в БД, при старте приложения

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ import uvicorn
 
 from app.api.v1.auth import router as auth_router
 from app.api.v1.test_routes import router as test_router
+from app.database.db import init_db
 
 app = FastAPI(
             title="Сервис авторизации",
             version="0.1",
 )
+
+init_db() # Инициализация таблиц в БД, при старте приложения
 
 app.include_router(auth_router, prefix="/API/v0.1")
 app.include_router(test_router, prefix="/API/v0.1")
