bcd07fda4941148001effa3150d7589ce27e31f2
Evgeny <jacksonou3@gmail.com>
2025-05-19 10:39:58 +0300

    Добавление в приложение lifespan. Проверка создания таблиц при старте приложения

--- app/main.py
+++ app/main.py
@@ -6,19 +6,18 @@ from app.api.v1.auth import router as auth_router
 from app.api.v1.test_routes import router as test_router
 from app.database.db import init_db
 
-app = FastAPI(
-            title="Сервис авторизации",
-            version="0.1",
-)
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()
     yield
     # Код, выполняемый при остановке (если нужен)
     print("Application shutdown")
-    
 
+app = FastAPI(
+            title="Сервис авторизации",
+            version="0.1",
+            lifespan=lifespan  # Укажи lifespan здесь
+)
 
 app.include_router(auth_router, prefix="/API/v0.1")
 app.include_router(test_router, prefix="/API/v0.1")
