d5f8485e459b1490b75fb33d47cfe45ff1d420ba
Evgeny <jacksonou3@gmail.com>
2025-05-21 09:28:23 +0300

    ♻️(db): Рефакторинг кода, Logger вместо print

--- app/database/db.py
+++ app/database/db.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from typing import Annotated, Generator
 
 from app.core.config import settings
+from app.core.logger import logger
 from app.models.user import Base
 
 # Создание движка SQLAlchemy
@@ -20,11 +21,11 @@ SessionLocal = sessionmaker(
 def init_db():
     """Создаёт все таблицы в базе данных на основе моделей."""
     try:
-        print(f"Попытка создания таблиц с движком: {engine}")
+        logger.info(f"Попытка создания таблиц с движком: {engine}")
         Base.metadata.create_all(bind=engine)
-        print("✅ Таблицы успешно созданы.")
+        logger.info("✅ Таблицы успешно созданы.")
     except Exception as e:
-        print(f"❌ Ошибка создания таблиц: {e}")
+        logger.warning(f"❌ Ошибка создания таблиц: {e}")
         raise
 
 def get_session() -> Generator[Session, None, None]:
