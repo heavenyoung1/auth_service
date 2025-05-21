a2007555cd94d0ca15288a8f5cd09287f2c5ce9f
Evgeny <jacksonou3@gmail.com>
2025-05-21 10:09:53 +0300

    ✨(User, Token): Рефакторинг и добавление нового функционала

--- app/models/user.py
+++ app/models/user.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Literal, List
 
 from app.models.base import Base
-#from app.models.token import RefreshToken
 
 class User(Base):
+    """ Модель пользователя для управления аутентификацией и авторизацией """
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    login: Mapped[str] = mapped_column(String(30), nullable=False, info={"min_length": 6}) # Минимальная длина - 6 символов
+
+    login: Mapped[str] = mapped_column(String(30), nullable=False, unique=True, index=True, info={"min_length": 6}) # Минимальная длина - 6 символов
     fullname: Mapped[str] = mapped_column(String(100))
     hashed_password: Mapped[str] = mapped_column(String(128), nullable=False)
     role: Mapped[Literal["admin", "user"]] = mapped_column(String, default="user")
@@ -18,7 +19,6 @@ class User(Base):
 
     refresh_tokens: Mapped[List["RefreshToken"]] =  relationship(back_populates="user", cascade="all, delete") # Добавлен List, теперь связь - One To Many
 
-    # Для отладки определяется метод repr
     def __repr__(self) -> str:
         return f"Метод __repr__: User(id={self.id!r}, login={self.login!r}, fullname={self.fullname!r})"
 
