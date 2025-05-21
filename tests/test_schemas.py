a468a256f790e3e5d88cc5edefb6aaa7af0b89f1
Evgeny <jacksonou3@gmail.com>
2025-05-21 16:49:27 +0300

    🧪(tests): Убраны лишние импорты

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,10 +1,6 @@
-from pydantic import ValidationError
 from sqlalchemy.orm.session import Session
-from app.models.token import RefreshToken
+
 from app.schemas.user import UserCreate
-from tests.conftest import test_session
-from app.core.logger import logger
-from datetime import datetime, timezone, timedelta
 
 
 def test_user_create_valid(test_session: Session):
