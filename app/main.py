dd851f0ed0bfd392b256767aafc1c9669e08a14a
Evgeny <jacksonou3@gmail.com>
2025-05-21 13:11:11 +0300

    ♻️(main) Рефакторинг

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 
 from app.api.v1.auth import router as auth_router
-from app.api.v1.test_routes import router as test_router
+from app.api.v1.base_routes import router as base_router
 from app.database.db import init_db
 
 @asynccontextmanager
@@ -16,11 +16,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
             title="Сервис авторизации",
             version="0.1",
-            lifespan=lifespan  # Укажи lifespan здесь
+            lifespan=lifespan
 )
 
 app.include_router(auth_router, prefix="/API/v0.1")
-app.include_router(test_router, prefix="/API/v0.1")
+app.include_router(base_router, prefix="/API/v0.1")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
