98d8e668bcf575f4f2a04a1bdf525cbda87e1503
Evgeny <jacksonou3@gmail.com>
2025-05-22 10:17:59 +0300

    Реализован тест, для несуществующего refresh-token

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -190,6 +190,20 @@ def test_error_creating_refresh_token(client, user_factory):
         assert login_json["detail"] == "Ошибка создания refresh-token!"
         logger.info(login_json)
 
+def test_refresh_token_not_found(client, user_factory):
+    user = user_factory()  # Создание данных пользователя из класса UserData
+    client.post("/API/v0.1/register", json=user.__dict__)
+    client.post("/API/v0.1/login", data={
+        "username": user.login,
+        "password": user.password
+        }
+    )
+    refresh_response = client.post("/API/v0.1/refresh", json={
+        "refresh_token": "QWERTY12345",
+        }
+    )
+    assert refresh_response.status_code == 404
+    assert refresh_response.json()["detail"] == "Refresh-токен не найден"
 
 def test_logout_user(client, user_factory):
     """Тест - выход пользователя из сессии"""
