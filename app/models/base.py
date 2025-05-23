dab5c1d22c0d193007f0bb101dc4423259cd1ed4
Evgeny <jacksonou3@gmail.com>
2025-05-23 15:49:53 +0300

    Добавление новой миграции с новыми таблицами

--- app/models/base.py
+++ app/models/base.py
@@ -3,3 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 class Base(DeclarativeBase):
     """Базовый класс для всех моделей приложения """
     pass
+
+# Импортируем модели, чтобы они добавлялись в Base.metadata
+# from app.models.user import User
+# from app.models.token import RefreshToken
